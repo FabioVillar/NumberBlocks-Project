af72c1151e5ded4c29900decfa98c08d180fe0a2
FabioVillar <63696039+FabioVillar@users.noreply.github.com>
2020-07-03 17:40:49 -0300

    Delete IMovement.java

--- src/src/IMovement.java
+++
@@ -1,8 +0,0 @@
-package Arquivos1;
-
-public interface IMovement {
-    public boolean goRight(CBoard board, boolean verify);
-    public boolean goLeft(CBoard board, boolean verify);
-    public boolean goUp(CBoard board, boolean verify);
-    public boolean goDown(CBoard board, boolean verify);
-}
