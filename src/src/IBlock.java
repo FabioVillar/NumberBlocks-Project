89d54aa09fcefb4029de0f9578ae182e6aee99d5
FabioVillar <63696039+FabioVillar@users.noreply.github.com>
2020-07-05 12:08:07 -0300

    Delete IBlock.java

--- src/src/IBlock.java
+++
@@ -1,8 +0,0 @@
-package Arquivos1;
-public interface IBlock {
-    public int getValue();
-    public boolean blockToRight(CBoard board, int line, int column, boolean verify);
-    public boolean blockToLeft(CBoard board, int line, int column, boolean verify);
-    public boolean blockToUp(CBoard board, int line, int column, boolean verify);
-    public boolean blockToDown(CBoard board, int line, int column, boolean verify);
-}
